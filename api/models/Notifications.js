ae67ba41df487620c36e752c08417721598813c3
SADmammoth <lo.ma200018@gmail.com>
2021-03-08 17:28:33 +0300

    init

---
+++ api/models/Notifications.js
@@ -0,0 +1,16 @@
+const _ = require("@sailshq/lodash");
+
+module.exports = {
+  attributes: {
+    title: { type: "string", required: true },
+    time: {
+      type: "ref",
+      columnType: "datetime",
+      required: true,
+      custom: (val) => {
+        return _.isDate(val);
+      },
+    },
+    author: { type: "string", required: true },
+  },
+};
--- api/models/Products.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  attributes: {
-    nameOnMenu: { type: "string", required: true },
-    price: { type: "string", required: true },
-    percentRealMeat: { type: "number", defaultsTo: 20, columnType: "FLOAT" },
-    numCalories: { type: "number" },
-  },
-};
