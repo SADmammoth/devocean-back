e72317b3c9021fe22c6a36b4dbc88914528bf622
SADmammoth <lo.ma200018@gmail.com>
2021-03-09 01:01:03 +0300

    config: minor tweeks

--- api/models/Notifications.js
+++ api/models/Notifications.js
@@ -1,16 +1,16 @@
-const _ = require("@sailshq/lodash");
+const _ = require('@sailshq/lodash');
 
 module.exports = {
   attributes: {
-    title: { type: "string", required: true },
+    title: { type: 'string', required: true },
     time: {
-      type: "ref",
-      columnType: "datetime",
+      type: 'ref',
+      columnType: 'datetime',
       required: true,
       custom: (val) => {
         return _.isDate(val);
       },
     },
-    author: { type: "string", required: true },
+    author: { type: 'string', required: true },
   },
 };
